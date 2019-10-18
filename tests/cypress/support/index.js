740147d80c63cee1967ab79fb1c56ff69f35c169
termcaps <avallete@student.42.fr>
2019-10-18 17:10:15 +0200

    feat(tests): Add and setup cypress as testing tool

---
+++ tests/cypress/support/index.js
@@ -0,0 +1,17 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// We don't need custom commands as we use TypeScript and can 
+// create simple modules that we can import with more typesafety
