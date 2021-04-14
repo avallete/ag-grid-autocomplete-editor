49dd1ca52851bc43d6086ed0446124ef11603201
avallete <avallete@student.42.fr>
2021-04-14 14:08:05 +0200

    add clean script and remove build artefacts

--- autocompleter/rendering.d.ts
+++
@@ -1,5 +0,0 @@
-import { AutocompleteItem, RenderFunction, RenderGroupFunction } from './types';
-export declare function removeChildren(container: Node): void;
-export declare function defaultRenderGroup(groupName: string): HTMLDivElement;
-export declare function defaultRender(item: AutocompleteItem): HTMLDivElement;
-export declare function renderItems<T extends AutocompleteItem>(items: T[], selected: T | undefined, inputValue: string, render: RenderFunction<T>, renderGroup: RenderGroupFunction, clickHandler: (item: T, event: MouseEvent) => void): DocumentFragment;
